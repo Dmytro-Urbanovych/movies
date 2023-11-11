fd0e84126c67895d3d9739bb0d81b8b96365ce4e
Dmytro-Urbanovych <bottle_water@ukr.net>
2023-11-11 21:39:06 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import AppBar from './components/AppBar';
+import Container from './components/Container';
+import Loader from './components/Loader';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Статические импорты
+// import HomePage from './views/HomePage';
+// import MoviesPage from './views/MoviesPage';
+// import MovieDetailsPage from './views/MovieDetailsPage';
+
+// Динамические импорты
+const HomePage = lazy(() =>
+    import('./views/HomePage' /* webpackChunkName: "home-page" */),
+);
+const MoviesPage = lazy(() =>
+    import('./views/MoviesPage' /* webpackChunkName: "movies-page" */),
+);
+const MovieDetailsPage = lazy(() =>
+    import(
+        './views/MovieDetailsPage' /* webpackChunkName: "movie-details-page" */
+    ),
+);
+
+export default function App() {
+    return (
+        <Container>
+            <ToastContainer />
+            <AppBar />
+
+            <Suspense fallback={<Loader />}>
+                <Switch>
+                    <Route path="/" exact>
+                        <HomePage />
+                    </Route>
+
+                    <Route path="/movies" exact>
+                        <MoviesPage />
+                    </Route>
+
+                    <Route path="/movies/:slug">
+                        <MovieDetailsPage />
+                    </Route>
+                </Switch>
+            </Suspense>
+        </Container>
+    );
 }
-
-export default App;
